4fcb92a45de5bf53894fb09c34e65f66652cb0c1
Mohamed yousuf harris <mohamed23bcs13@iiitkottayam.ac.in>
2026-03-24 11:28:44 +0530

    corrected the url

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/student-details', methods=['GET'])
+@app.route('/student-details')
 def get_details():
-    data = {
+    return jsonify({
         "name": "Yousuf harris",
         "roll_number": "2023BCS0013"
-    }
-    return jsonify(data)
+    })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000)
